5fe9aec28000ee2e1315b18ae27693292bef04d5
Johan Pretorius <rian7809@gmail.com>
2022-03-17 18:32:49 +0200

    Initial Commit

---
+++ Learn GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn GH
+//
+//  Created by Johan Pretorius on 2022/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn GH/Learn_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_GHApp.swift
+//  Learn GH
+//
+//  Created by Johan Pretorius on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct Learn_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
